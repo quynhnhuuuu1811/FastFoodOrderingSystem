17906d2ef8c0d2e99bbb55ec371bc9fa0e4c043c
ADMIN <tranminhtri10213@gmail.com>
2024-09-07 21:59:43 +0700

    add firebase set up manage food

--- lib/features/foods/data/food_api_client.dart
+++ lib/features/foods/data/food_api_client.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:fastfood_ordering_system/features/foods/dtos/food_request_dto.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../dtos/food_dto.dart';
@@ -17,6 +18,116 @@ class FoodApiClient {
       );
 
       // Kiểm tra phản hồi và dữ liệu
+      if (response.statusCode == 200) {
+        final List<dynamic> data = response.data;
+        return data.map((json) => FoodDto.fromJson(json)).toList();
+      } else {
+        log('Failed with status code: ${response.statusCode}');
+        throw Exception('Failed to load foods. Status code: ${response.statusCode}');
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        log('DioException: ${e.message}');
+        log('Response data: ${e.response!.data}');
+        throw Exception(e.response!.data['message'] ?? 'Unknown error');
+      } else {
+        log('DioException without response: ${e.message}');
+        throw Exception(e.message);
+      }
+    } catch (e) {
+      log('Unknown exception: $e');
+      throw Exception('Unknown error occurred');
+    }
+  }
+
+  Future<FoodDto> addFood(FoodRequestDto food) async {
+    try {
+      final response = await dio.post(
+        '/v1/foods',
+        data: food.toJson(),
+      );
+
+      if (response.statusCode == 200) {
+        return FoodDto.fromJson(response.data);
+      } else {
+        log('Failed with status code: ${response.statusCode}');
+        throw Exception('Failed to add food. Status code: ${response.statusCode}');
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        log('DioException: ${e.message}');
+        log('Response data: ${e.response!.data}');
+        throw Exception(e.response!.data['message'] ?? 'Unknown error');
+      } else {
+        log('DioException without response: ${e.message}');
+        throw Exception(e.message);
+      }
+    } catch (e) {
+      log('Unknown exception: $e');
+      throw Exception('Unknown error occurred');
+    }
+  }
+
+  Future<void> deleteFood(int id) async {
+    try {
+      final response = await dio.delete(
+        '/v1/foods/$id',
+      );
+
+      if (response.statusCode != 200) {
+        log('Failed with status code: ${response.statusCode}');
+        throw Exception('Failed to delete food. Status code: ${response.statusCode}');
+      }
+
+    } on DioException catch (e) {
+      if (e.response != null) {
+        log('DioException: ${e.message}');
+        log('Response data: ${e.response!.data}');
+        throw Exception(e.response!.data['message'] ?? 'Unknown error');
+      } else {
+        log('DioException without response: ${e.message}');
+        throw Exception(e.message);
+      }
+    } catch (e) {
+      log('Unknown exception: $e');
+      throw Exception('Unknown error occurred');
+    }
+  }
+
+  Future<FoodDto> updateFood(FoodRequestDto food) async {
+    try {
+      final response = await dio.put(
+        '/v1/foods/${food.id}',
+        data: food.toJson(),
+      );
+
+      if (response.statusCode == 200) {
+        return FoodDto.fromJson(response.data);
+      } else {
+        log('Failed with status code: ${response.statusCode}');
+        throw Exception('Failed to update food. Status code: ${response.statusCode}');
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        log('DioException: ${e.message}');
+        log('Response data: ${e.response!.data}');
+        throw Exception(e.response!.data['message'] ?? 'Unknown error');
+      } else {
+        log('DioException without response: ${e.message}');
+        throw Exception(e.message);
+      }
+    } catch (e) {
+      log('Unknown exception: $e');
+      throw Exception('Unknown error occurred');
+    }
+  }
+
+  Future<List<FoodDto>> fetchFoods() async {
+    try {
+      final response = await dio.get(
+        '/v1/foods',
+      );
+
       if (response.statusCode == 200) {
         final List<dynamic> data = response.data;
         return data.map((json) => FoodDto.fromJson(json)).toList();
